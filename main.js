ab93d070337e8063356e7d8cb33b27a74b441bdc
Bryan Liu <liubryan.developer@gmail.com>
2020-08-18 15:15:34 -0400

    bug fix

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function sendWelcomeMessage(voiceState) {
     voiceState.guild.channels.cache.forEach((channel) => { 
          if (channel.type == 'text') {
             channel.send("Welcome to China " + newMember.member.displayName + "." + " Your social credit score has been set to 100. Enjoy your stay.")
-            break
+            return
         }   
     })
 }
